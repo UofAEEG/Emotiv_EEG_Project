b49ac978eb2f982c98528d7680e687c821b0ab7d
Mark Galloway <mgallowa@ualberta.ca>
2013-02-20 23:35:41 -0700

    ( :

--- EEG/src/Raw/RawData.java
+++ EEG/src/Raw/RawData.java
@@ -2,14 +2,8 @@ package Raw;
 
 public class RawData {
 	      
-	
 	public static void main(String[] args) {
-		System.out.println("1");
-		
-		String path = System.getProperty("java.library.path");
-	    System.out.println(path);
-	    
-	    System.load("C://edk.dll");
+		System.out.println("Hello Emotiv World");
 	}
 	
 }
