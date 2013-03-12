2e38040bd9624ada8e8982a6fc0d8a3cd73a8238
Mark Galloway <mgallowa@ualberta.ca>
2013-03-12 16:51:33 -0600

    Fixed indentation

--- EEG/src/Raw/Matrix.java
+++ EEG/src/Raw/Matrix.java
@@ -8,41 +8,39 @@ import java.io.IOException;
 // double [sample] [sensor]
 //{[a,b,c,...],[a',b',c',...],[a",b",c",..]}
 public class Matrix {
+
+	final int MATRIX_SIZE = 128;
+	final int NUM_SENSORS = 14;
+	double[][] matrix = null;
+	int matrixNo = 0;
 	
+	public Matrix() {
+	    super();
+	    matrix = new double[MATRIX_SIZE][NUM_SENSORS];
+	}
 
 
-final int MATRIX_SIZE = 128;
-final int NUM_SENSORS = 14;
-double[][] matrix = null;
-int matrixNo = 0;
-
-public Matrix() {
-    super();
-    matrix = new double[MATRIX_SIZE][NUM_SENSORS];
-}
-
-
-/*Write the matrix's data to a text file*/
-public void toFile() {
-    
-    BufferedWriter matrixout = null;
-    
-    try {
-        matrixout = new BufferedWriter(new FileWriter("data/Matrix_" + matrixNo + "_" + RawData.fileName));
-    
-    
-    for (int i=0; i<MATRIX_SIZE; i++) {
-        for (int j=0; j<14; j++){
-            matrixout.write(Double.toString(matrix[i][j])+" ");
-        }
-        matrixout.write("\n");
-    }
-    matrixout.write("\n\n\n\n");
-        
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-        matrixNo++;
+	/*Write the matrix's data to a text file*/
+	public void toFile() {
+	    
+	    BufferedWriter matrixout = null;
+	    
+	    try {
+	        matrixout = new BufferedWriter(new FileWriter("data/Matrix_" + matrixNo + "_" + RawData.fileName));
+	    
+	    
+	    for (int i=0; i<MATRIX_SIZE; i++) {
+	        for (int j=0; j<14; j++){
+	            matrixout.write(Double.toString(matrix[i][j])+" ");
+	        }
+	        matrixout.write("\n");
+	    }
+	    matrixout.write("\n\n\n\n");
+	        
+	        } catch (IOException e) {
+	            System.err.println(e.getMessage());
+	        }
+	        matrixNo++;
     }
     
 }
