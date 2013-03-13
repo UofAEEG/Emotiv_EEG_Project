8f9e0acbe64a6ee81b6efdbb5764d932481528ee
Mark Galloway <mgallowa@ualberta.ca>
2013-03-13 11:10:40 -0600

    added 2nd Matrix constructor

--- EEG/src/Raw/Matrix.java
+++ EEG/src/Raw/Matrix.java
@@ -11,12 +11,20 @@ public class Matrix {
 
 	final int MATRIX_SIZE = 128;
 	final int NUM_SENSORS = 14;
+	int numSeconds;
 	double[][] matrix = null;
 	int matrixNo = 0;
 	
 	public Matrix() {
 	    super();
 	    matrix = new double[MATRIX_SIZE][NUM_SENSORS];
+	    this.numSeconds = 1;
+	}
+	
+	public Matrix(int numSeconds) {
+	    super();
+	    matrix = new double[MATRIX_SIZE * numSeconds][NUM_SENSORS];
+	    this.numSeconds = numSeconds;
 	}
 
 
@@ -29,8 +37,8 @@ public class Matrix {
 	        matrixout = new BufferedWriter(new FileWriter("data/Matrix_" + matrixNo + "_" + RawData.fileName));
 	    
 	    
-	    for (int i=0; i<MATRIX_SIZE; i++) {
-	        for (int j=0; j<14; j++){
+	    for (int i=0; i<MATRIX_SIZE*numSeconds; i++) {
+	        for (int j=0; j<NUM_SENSORS; j++){
 	            matrixout.write(Double.toString(matrix[i][j])+" ");
 	        }
 	        matrixout.write("\n");
